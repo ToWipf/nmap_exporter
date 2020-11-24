16c13077e3db123713c369095a3e88da4265838a
ToWipf <tobf@web.de>
2020-11-25 00:43:25 +0100

    build

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker build . --file Dockerfile --tag docker.pkg.github.com/towipf/nmap_exporter:0.1
+docker buildx build --platform linux/arm -t docker.pkg.github.com/towipf/nmap_exporter:0.1arm -f Dockerfile.arm .
+
+docker push docker.pkg.github.com/towipf/nmap_exporter:0.1
+docker push docker.pkg.github.com/towipf/nmap_exporter:0.1arm
