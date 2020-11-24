3ec788d31b7c2eb84a4924ec36967a1e780db354
ToWipf <tobf@web.de>
2020-11-25 00:48:25 +0100

    build arm

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+apt install qemu-user
+DOCKER_CLI_EXPERIMENTAL=enabled 
+
 docker build . --file Dockerfile --tag docker.pkg.github.com/towipf/nmap_exporter:0.1
 docker buildx build --platform linux/arm -t docker.pkg.github.com/towipf/nmap_exporter:0.1arm -f Dockerfile.arm .
 
