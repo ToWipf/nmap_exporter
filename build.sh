9fa45ef5d4783fd15e00cedfe5a45389b01834fb
ToWipf <tobf@web.de>
2020-11-25 01:03:39 +0100

    build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo apt-get install qemu-user -y
 DOCKER_CLI_EXPERIMENTAL=enabled 
+DOCKER_BUILDKIT=1
+BUILDX_NO_DEFAULT_LOAD=arm32v7
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 echo "_______Build_______"
