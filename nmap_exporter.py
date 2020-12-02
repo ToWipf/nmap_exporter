26826be97b8637752014a5933400b218f54797ba
ToWipf <tobf@web.de>
2020-12-02 17:51:05 +0100

    version 0.2

--- nmap_exporter.py
+++ nmap_exporter.py
@@ -1,53 +1,49 @@
 #!/usr/bin/env python
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
-from SocketServer import ThreadingMixIn
-import threading
-import subprocess
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from subprocess import check_output
 
-
-def doScan(ip):
+def doScan(ip, dns):
     output = []
-    ping_command = '{} -sP --host-timeout 10 --max-retries 5 --dns-servers 192.168.2.3 {} -oG n.tmp'.format(
-        "nmap", ip)
+    ping_command = '/usr/bin/nmap -sP --host-timeout 10 --max-retries 5 --dns-servers {} {} -oG n.tmp'.format(dns, ip)
 
     # Execute and write output to file
-    subprocess.Popen(ping_command, stdout=subprocess.PIPE,
-                     shell=True).communicate()
+    try:
+        check_output(ping_command, shell=True, timeout=5)
+    except Exception:
+        pass
+        fout = "{ip=\"" + "scan_failed" + "\",hostname=\"" + "scan_failed"+ "\"}"
+        output.append("nmap_scan{} 1".format(fout))
+        return output
 
-    # read file
-    cmd_output = str(subprocess.Popen(
-        "cat n.tmp", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()[0])
+    with open("n.tmp") as f:
+        cmd_output = f.readlines()
 
     # Parse
-    for line in cmd_output.split("\n"):
+    for line in cmd_output:
         if "#" not in line:
             if line:
                 val = line.replace("\t", " ").split(" ")
-                out = "{ip=\"" + val[1] + "\",hostname=\"" + \
-                    val[2].replace("(", "").replace(")", "") + "\"}"
+                out = "{ip=\"" + val[1] + "\",hostname=\"" + val[2].replace("(", "").replace(")", "") + "\"}"
                 output.append("nmap_scan{} 1".format(out))
 
     output.append('')
     return output
 
 
-class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
-    """Handle requests in a separate thread."""
-
-
 class StartScan(BaseHTTPRequestHandler):
     def do_GET(self):
+        message = '\n'.join(doScan('192.168.2.*', '192.168.2.3'))
 
-        message = '\n'.join(doScan('192.168.2.*'))
-        # Prepare HTTP status code
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(message)
+        self.wfile.write(message.encode())
         return
-
 
 if __name__ == '__main__':
     port = 80
-    print('Starting toWipf device scanner Version 0.1 on port {}'.format(port))
-    server = ThreadedHTTPServer(('0.0.0.0', port), StartScan)
-    server.serve_forever()
+    httpdserver = HTTPServer(('0.0.0.0', port), StartScan)
+    try:
+        httpdserver.serve_forever()
+    except KeyboardInterrupt:
+        pass
+        httpdserver.server_close()
--- readme.md
+++ readme.md
@@ -6,10 +6,10 @@ Device scan for Prometheus
 
 ## Build and Debug:
 ```sh
-docker run -d --name nmap_exporter -p 9042:80 nmap_exporter
-
 docker build -t nmap_exporter .
 docker run -p 9042:80 nmap_exporter
+
+docker run -d --name nmap_exporter -p 9042:80 nmap_exporter
 ```
 
 ## Build Arm:
